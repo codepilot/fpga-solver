0f645cff539b625b97147ab2b2ec8d18bbee1be3
Daniel Kluss <codepilot@gmail.com>
2023-11-19 01:02:10 -0900

    remove old build methods

--- configure.cmd
+++
@@ -1,5 +0,0 @@
-cmake -D CMAKE_INSTALL_PREFIX=./OpenCL-Headers/install -S ./OpenCL-Headers -B ./OpenCL-Headers/build
-
-cmake -D CMAKE_PREFIX_PATH=C:\Users\root\Desktop\fpga-solver\OpenCL-Headers\install -D CMAKE_INSTALL_PREFIX=./OpenCL-ICD-Loader/install -S ./OpenCL-ICD-Loader -B ./OpenCL-ICD-Loader/build 
-
-cmake -D WITH_ZLIB=OFF -S ./capnproto -B ./capnproto/build
