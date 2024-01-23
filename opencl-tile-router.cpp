575909f28a437257a13ffd3814957b3dd188cb26
Daniel Kluss <codepilot@gmail.com>
2024-01-22 17:43:40 -0800

    add device file to router

--- opencl-tile-router.cpp
+++ opencl-tile-router.cpp
@@ -1,8 +1,9 @@
 #include "ocl_tile_router.h"
 
 int main() {
+	DevFlat dev{ "_deps/device-file-src/xcvu3p.device" };
 	PhysGZ phys{ "_deps/benchmark-files-src/boom_med_pb_unrouted.phys" };
-	auto ocltr{ OCL_Tile_Router::make(phys.root) };
+	auto ocltr{ OCL_Tile_Router::make(dev.root, phys.root) };
 	ocltr.step_all(0).value();
 	for (auto&& queue : ocltr.queues) {
 		queue.finish().value();
