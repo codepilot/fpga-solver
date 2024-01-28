be43db377d2cf04fe2c2e297c202139068c6fb53
Daniel Kluss <codepilot@gmail.com>
2024-01-27 18:47:03 -0800

    add file to cl pip files

--- cl_pip_files.cmake
+++ cl_pip_files.cmake
@@ -4,14 +4,17 @@ block()
   set(pip_count_offset ${PROJECT_BINARY_DIR}/pip_count_offset.bin)
   set(pip_tile_body ${PROJECT_BINARY_DIR}/pip_tile_body.bin)
   set(pip_body ${PROJECT_BINARY_DIR}/pip_body.bin)
+  set(inverse_wires ${PROJECT_BINARY_DIR}/Inverse_Wires.bin)
 
   list(APPEND cl_pip_file_set ${pip_count_offset})
   list(APPEND cl_pip_file_set ${pip_tile_body})
   list(APPEND cl_pip_file_set ${pip_body})
+  list(APPEND cl_pip_file_set ${inverse_wires})
 
   message("pip_count_offset = ${pip_count_offset}")
   message("pip_tile_body = ${pip_tile_body}")
   message("pip_body = ${pip_body}")
+  message(("inverse_wires = ${inverse_wires}"))
 
   add_custom_command(
     OUTPUT ${cl_pip_file_set}
