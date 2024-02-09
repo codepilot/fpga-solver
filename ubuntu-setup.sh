1b0063c2ed749760bdeebc6d48d7fae4896c5e1c
Daniel Kluss <codepilot@gmail.com>
2024-02-08 22:59:33 -0800

    fix for clang linux

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -14,7 +14,7 @@ add-apt-repository -y 'deb-src [trusted=yes] http://apt.llvm.org/jammy/ llvm-too
 add-apt-repository -y 'deb-src [trusted=yes] http://apt.llvm.org/jammy/ llvm-toolchain-jammy-17 main'
 add-apt-repository -y 'deb-src [trusted=yes] http://apt.llvm.org/jammy/ llvm-toolchain-jammy-18 main'
 apt update
-apt install -y wget python3-dev libpython3-dev build-essential ocl-icd-libopencl1 git pkg-config libclang-${LLVM_VERSION}-dev clang-${LLVM_VERSION} llvm-${LLVM_VERSION} make ninja-build ocl-icd-libopencl1 ocl-icd-dev ocl-icd-opencl-dev libhwloc-dev zlib1g zlib1g-dev clinfo dialog apt-utils libxml2-dev libclang-cpp${LLVM_VERSION}-dev libclang-cpp${LLVM_VERSION} llvm-${LLVM_VERSION}-dev
+apt install -y wget python3-dev libpython3-dev build-essential ocl-icd-libopencl1 git pkg-config libclang-${LLVM_VERSION}-dev clang-${LLVM_VERSION} llvm-${LLVM_VERSION} make ninja-build ocl-icd-libopencl1 ocl-icd-dev ocl-icd-opencl-dev libhwloc-dev zlib1g zlib1g-dev clinfo dialog apt-utils libxml2-dev libclang-cpp${LLVM_VERSION}-dev libclang-cpp${LLVM_VERSION} llvm-${LLVM_VERSION}-dev libc++-${LLVM_VERSION}-dev libc++abi-${LLVM_VERSION}-dev
 git clone https://github.com/pocl/pocl.git
 cd pocl
 mkdir build
